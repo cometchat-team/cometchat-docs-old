f587dc65dc413abfd9d318cc597ab84381cc7b05
AhmedCoolProjects <bargadyahmed2000@gmail.com>
2023-08-02 17:58:46 +0100

    cleaning

--- src/client/define-ui-kit.js
+++ src/client/define-ui-kit.js
@@ -1,20 +1,6 @@
-import { defineCustomElements } from "@dytesdk/ui-kit/loader";
+// add a loader to prevent Page not found before it's rendered
+import react from "react";
 
-defineCustomElements();
-
-function onLoad() {
-  if (typeof window === "undefined") return;
-
-  const play = window.HTMLAudioElement.prototype.play;
-
-  // override play() to avoid autoplay modal
-  window.HTMLAudioElement.prototype.play = function () {
-    try {
-      play.call(this);
-    } catch (err) {
-      // pass
-    }
-  };
+export default function Loader() {
+  return react.createElement("div", null, "Loading...");
 }
-
-onLoad();
