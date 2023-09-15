4cbccfef9245e60db8c5511e48590eb15d995270
Ahmed BARGADY <bargadyahmed2000@gmail.com>
2023-09-15 13:06:38 +0100

    footer spacing and font, done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -30,28 +30,28 @@ const SOCIAL_MEDIA = [
 
 function Footer() {
   return (
-    <div className="flex w-full flex-row items-center justify-center bg-[#0A0914] py-6 text-[#FAFAFF]">
+    <div className="flex w-full flex-row items-center justify-center bg-[#0A0914] py-6 text-[#FAFAFF] text-opacity-[0.74]">
       <div className="flex w-full max-w-[1440px]   flex-row items-center justify-between px-[48px] text-sm xlg:px-0 ">
         {/* links */}
-        <div className="m-0 flex flex-row space-x-3">
+        <div className="m-0 text-xs flex flex-row space-x-3">
           <p className="m-0">2023 © CometChat</p>
-          <a className="m-0 text-[#FAFAFF]">Terms of Use</a>
-          <a className="m-0 text-[#FAFAFF]">Privacy Policy</a>
+          <a className="m-0 text-[#FAFAFF] text-opacity-[0.74]">Terms of Use</a>
+          <a className="m-0 text-[#FAFAFF] text-opacity-[0.74]">Privacy Policy</a>
         </div>
         {/* social media */}
         <div className="flex flex-row items-center space-x-4">
           {SOCIAL_MEDIA.map((item) => (
             <a
               key={item.title}
-              className="flex flex-row items-center justify-center space-x-1 text-[#fafaff]">
+              className="flex flex-row text-xs items-center justify-center space-x-1 text-[#FAFAFF] text-opacity-[0.74]">
               <img
                 className={`${
-                  item.title === "X" ? "h-4" : "h-5"
+                  item.title === "X" ? "h-3" : "h-4"
                 } object-contain`}
                 src={`/imgs/icons/${item.icon}`}
                 alt={item.title}
               />
-              <p className="m-0 text-sm">{item.title}</p>
+              <p className="m-0 text-xs">{item.title}</p>
             </a>
           ))}
         </div>
