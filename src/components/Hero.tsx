c29bf9be8e41b140f6f1fa14028c5198b547306f
Ahmed BARGADY <bargadyahmed2000@gmail.com>
2023-09-18 23:28:02 +0100

    testing the dark cless from tailwindcss, removing tailwing from building dependencies

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import React from "react";
 
 const DATA = [
@@ -58,24 +59,31 @@ function Hero() {
       <img
         src="/imgs/planets.svg"
         alt="Planets"
-        className="absolute right-0 top-0 h-40 md:h-[230px] md:w-[230px] object-contain"
+        className="absolute right-0 top-0 h-40 object-contain md:h-[230px] md:w-[230px]"
       />
       <img
         src="/imgs/planets1.svg"
         alt="Planets"
-        className="absolute z-50 -left-[9%] md:-left-[5%] bottom-16 h-40 md:h-[270px] w-40 md:w-[270px] object-contain"
+        className="absolute -left-[9%] bottom-16 z-50 h-40 w-40 object-contain md:-left-[5%] md:h-[270px] md:w-[270px]"
       />
       {/* middle */}
-      <div className="mt-16 mb-6 flex w-3/4 md:w-1/2 max-w-[1440px] flex-col items-center justify-center text-center">
-        <h1 className="text-4xl font-extrabold">Get Started</h1>
-        <p className="p--cust  font--custom--thin">
+      <div className="mb-6 mt-16 flex w-3/4 max-w-[1440px] flex-col items-center justify-center text-center md:w-1/2">
+        <h1 className="text-4xl font-extrabold text-blue-500 dark:text-red-500">
+          Get Started
+        </h1>
+        <p
+          className={clsx(
+            "p--cust  font--custom--thin text-blue-500",
+            "dark:text-red-500"
+          )}
+        >
           Seamlessly integrate real-time chat, voice, and video
           functionalities.Begin your Cometchat implementation by selecting the
           technologies that best suit your organizational needs.
         </p>
       </div>
       {/* Cards */}
-      <div className="grid w-11/12 md:w-[calc(100%-352px)]  max-w-[1440px] grid-cols-2 md:grid-cols-4 gap-3 md:gap-7">
+      <div className="grid w-11/12 max-w-[1440px]  grid-cols-2 gap-3 md:w-[calc(100%-352px)] md:grid-cols-4 md:gap-7">
         {DATA.map((item) => (
           <CardItem key={item.title} {...item} />
         ))}
@@ -89,7 +97,8 @@ function Hero() {
           className="ml-2"
           viewBox="0 0 14 10"
           fill="none"
-          xmlns="http://www.w3.org/2000/svg">
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <path
             d="M1 5H13M13 5L9.14286 1M13 5L9.14286 9"
             stroke="#6852D6"
@@ -108,25 +117,26 @@ export default Hero;
 
 function CardItem({ icon, title }: { icon: string; title: string }) {
   return (
-    <div className="flex flex-col items-start rounded-[20px] border border-solid border-[#14131D14] bg-[#F3F3F8] py-3 pr-3 pl-4">
+    <div className="flex flex-col items-start rounded-[20px] border border-solid border-[#14131D14] bg-[#F3F3F8] py-3 pl-4 pr-3">
       <img
         src={`/imgs/logos/${icon}`}
         alt={title}
         className="h-12 w-12 object-contain grayscale"
       />
-      <h3 className="mt-1 text-[18px] font-bold mb-0">{title}</h3>
+      <h3 className="mb-0 mt-1 text-[18px] font-bold">{title}</h3>
     </div>
   );
 }
 
 function ADCard() {
   return (
-    <div className="absolute -bottom-[140px] md:-bottom-[95px]  left-1/2 z-10 flex w-11/12 md:w-3/4 max-w-[1440px] -translate-x-1/2 transform flex-col items-center justify-center rounded-[32px] bg-white">
+    <div className="absolute -bottom-[140px] left-1/2  z-10 flex w-11/12 max-w-[1440px] -translate-x-1/2 transform flex-col items-center justify-center rounded-[32px] bg-white md:-bottom-[95px] md:w-3/4">
       <div
-        className="relative flex h-[280px] md:h-[190px] w-full flex-col items-start overflow-hidden rounded-[32px] bg-[#14131D0F] border border-solid border-[#14131D0A] bg-opacity-10  px-4 pt-6 md:px-7"
+        className="relative flex h-[280px] w-full flex-col items-start overflow-hidden rounded-[32px] border border-solid border-[#14131D0A] bg-[#14131D0F] bg-opacity-10 px-4  pt-6 md:h-[190px] md:px-7"
         style={{
           boxShadow: "0px 4px 20px 0px #14131D0F",
-        }}>
+        }}
+      >
         <h1 className="mb-1 text-2xl">Introducing CometChat UI Kits v4 Beta</h1>
         <p className="font--custom--thin p--cust">
           V4 UI kits offer a modular design, extended functionality, and
@@ -138,16 +148,17 @@ function ADCard() {
           style={{
             textDecoration: "none",
           }}
-          className="mt-2 font--custom--thin font-thin
-      rounded-xl bg-[#6852D6]
-       px-7 py-[10px] text-xs text-[#FCFCFE]"
-          href="">
+          className="font--custom--thin mt-2 rounded-xl
+      bg-[#6852D6] px-7
+       py-[10px] text-xs font-thin text-[#FCFCFE]"
+          href=""
+        >
           Learn more
         </a>
         <img
           src="/imgs/planets2.svg"
           alt="Planets2"
-          className="absolute bottom-0 right-0 w-[180px] md:w-[250px] object-contain"
+          className="absolute bottom-0 right-0 w-[180px] object-contain md:w-[250px]"
         />
       </div>
     </div>
